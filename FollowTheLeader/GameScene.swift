33db9ee70633c5cfa5f109bb65e6f55804c128f7
roshando <allan.sequeira@gmail.com>
2015-07-27 09:09:40 -0500

    - added background sprite to scene - positioned the background sprite at the center of scene

--- FollowTheLeader/GameScene.swift
+++ FollowTheLeader/GameScene.swift
@@ -13,6 +13,21 @@ class GameScene: SKScene {
         /* Setup your scene here */
         
         backgroundColor = SKColor.whiteColor();
+        
+        // create (background) sprite
+        let background = SKSpriteNode(imageNamed: "background1");
+        
+        // Position the sprite
+        // by default SpriteKit positions sprites at (0,0) which in
+        // SpriteKit represents bottom left. Note this is different 
+        // from the UIKit coordinate system in iOS, where (0,0) represents
+        // the top left
+        // Positioning the center of the (background) sprite at the center of the screen
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2);
+        
+        // add (background) sprite to scene
+        addChild(background);
+        
     }
     
 }
