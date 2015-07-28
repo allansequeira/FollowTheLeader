139fffa78f7baefa32654b10ab966261669173c8
roshando <allan.sequeira@gmail.com>
2015-07-28 00:21:04 -0500

    - added zPosition for background sprite

--- FollowTheLeader/GameScene.swift
+++ FollowTheLeader/GameScene.swift
@@ -22,11 +22,21 @@ class GameScene: SKScene {
         // SpriteKit represents bottom left. Note this is different 
         // from the UIKit coordinate system in iOS, where (0,0) represents
         // the top left
-        // Positioning the center of the (background) sprite at the center of the screen
+        // Positioning the center of the (background) sprite at the center 
+        // of the screen
         background.position = CGPoint(x: size.width / 2, y: size.height / 2);
         
+        // set the (background) zPosition so SpriteKit will draw it before
+        // anything else you add to the scene (which will default to a 
+        // zPosition of 0
+        background.zPosition = -1;
+            
         // add (background) sprite to scene
         addChild(background);
+        
+        // get size of sprite
+        let mySize = background.size;
+        println("Size: \(mySize)");
         
     }
     
