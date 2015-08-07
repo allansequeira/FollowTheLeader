8391249041e0e40f57490bfb9cb1605d37405590
roshando <allan.sequeira@gmail.com>
2015-08-06 23:12:29 -0500

    - initial draft of math utils

---
+++ FollowTheLeader/MyUtils.swift
@@ -0,0 +1,18 @@
+//
+//  MyUtils.swift
+//  FollowTheLeader
+//
+//  Created by Roshan Sequeira on 8/6/15.
+//  Copyright (c) 2015 mcsyon. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func += (inout left: CGPoint, right: CGPoint) {
+    left = left + right
+}
