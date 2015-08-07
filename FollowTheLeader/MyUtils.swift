2f1750bea2c3b86c3925ab288caa39a000e64270
roshando <allan.sequeira@gmail.com>
2015-08-07 00:13:55 -0500

    - more functions to subtract, multiply, divide a CGPoint by another CGPoint - functions to multiple and divide points by scalar CGFloat values

--- FollowTheLeader/MyUtils.swift
+++ FollowTheLeader/MyUtils.swift
@@ -20,3 +20,39 @@ func += (inout left: CGPoint, right: CGPoint) {
 func - (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x - right.x, y: left.y - right.y)
 }
+
+func -= (inout left: CGPoint, right: CGPoint) {
+    left = left - right
+}
+
+func * (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+}
+
+func *= (inout left: CGPoint, right: CGPoint) {
+    left = left * right
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func *= (inout point: CGPoint, scalar: CGFloat) {
+    point = point * scalar
+}
+
+func / (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x / right.x, y: left.y / right.y)
+}
+
+func /= (inout left: CGPoint, right: CGPoint) {
+    left = left / right
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+func /= (inout point: CGPoint, scalar: CGFloat) {
+    point = point / scalar
+}
